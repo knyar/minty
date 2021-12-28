5e69de48523bac71b97e661754ae9ce2b68e69a9
Anton Tolchanov <commits@knyar.net>
2021-12-28 17:29:24 +0200

    Use ipv4 for ipvs since it does not listen on ipv6

--- config/default.js
+++ config/default.js
@@ -15,7 +15,7 @@ const config = {
     deploymentConfigFile: 'minty-deployment.json',
 
     // If you're running IPFS on a non-default port, update this URL. If you're using the IPFS defaults, you should be all set.
-    ipfsApiUrl: 'http://localhost:5001',
+    ipfsApiUrl: 'http://127.0.0.1:5001',
 
     // If you're running the local IPFS gateway on a non-default port, or if you want to use a public gatway when displaying IPFS gateway urls, edit this.
     ipfsGatewayUrl: 'http://localhost:8080/ipfs',
