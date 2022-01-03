cd02382e58efee9519f6169da69b10d8021d43e9
Anton Tolchanov <commits@knyar.net>
2022-01-03 11:28:32 +0200

    Add polygon endpoints to hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,26 @@
 require("@nomiclabs/hardhat-waffle");
 
+const privateKey = "...";
+
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
     solidity: "0.7.3",
 
-    defaultNetwork: 'localhost',
+    defaultNetwork: 'matic_mainnet',
+    // defaultNetwork: 'matic_testnet',
     networks: {
-        hardhat: {},
-        localhost: {},
+        matic_mainnet: {
+            url: "https://rpc-mainnet.maticvigil.com",
+            gasPrice: 30000000000,
+            accounts: [privateKey]
+        },
+        matic_testnet: {
+            url: "https://rpc-mumbai.maticvigil.com",
+            gasPrice: 8000000000,
+            accounts: [privateKey]
+        }
     }
 };
-
